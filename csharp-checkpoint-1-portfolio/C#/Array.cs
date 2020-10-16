eeedd4c1f3b78ff8dfee52e4df9c6a7705291fdc
FabGtte <41702805+FabGtte@users.noreply.github.com>
2020-10-16 14:00:06 +0200

    Add files via upload

---
+++ csharp-checkpoint-1-portfolio/C#/Array.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace Checkpoint
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            int[] myArray = { 1, 2, 3, 4, 5 };
+            Console.WriteLine(Somme(myArray));
+            Console.WriteLine(ArrayAverage(myArray));
+        }
+
+        public static int Somme(int[] myArray)
+        {
+            int Sum = 0;
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Sum = Sum + myArray[i];
+            }
+            return Sum;
+        }
+
+
+        public static int ArrayAverage(int[] myArray)
+        {
+            int Sum = Somme(myArray);
+            int Average = Sum/ myArray.Length;
+            return Average;
+        }
+}
+
+}
+
+    
+
+
+    
+
+
